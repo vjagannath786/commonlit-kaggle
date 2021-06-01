bbe17aa1e16b18fb12fe9f1066fde2ad5728939a
vjagannath786 <36917923+vjagannath786@users.noreply.github.com>
2021-06-01 20:07:22 +0530

    Update train_early.py
    Added differential learning rates for layers

--- src/train_early.py
+++ src/train_early.py
@@ -47,6 +47,29 @@ roberta_pred = None
 
 
 
+def get_optimizer_params(model):
+    # differential learning rate and weight decay
+    param_optimizer = list(model.named_parameters())
+    learning_rate = 2e-5
+    no_decay = ['bias', 'gamma', 'beta']
+    group1=['layer.0.','layer.1.','layer.2.','layer.3.']
+    group2=['layer.4.','layer.5.','layer.6.','layer.7.']    
+    group3=['layer.8.','layer.9.','layer.10.','layer.11.']
+    group_all=['layer.0.','layer.1.','layer.2.','layer.3.','layer.4.','layer.5.','layer.6.','layer.7.','layer.8.','layer.9.','layer.10.','layer.11.']
+    optimizer_parameters = [
+        {'params': [p for n, p in model.roberta.named_parameters() if not any(nd in n for nd in no_decay) and not any(nd in n for nd in group_all)],'weight_decay_rate': 0.01},
+        {'params': [p for n, p in model.roberta.named_parameters() if not any(nd in n for nd in no_decay) and any(nd in n for nd in group1)],'weight_decay_rate': 0.01, 'lr': learning_rate/2.6},
+        {'params': [p for n, p in model.roberta.named_parameters() if not any(nd in n for nd in no_decay) and any(nd in n for nd in group2)],'weight_decay_rate': 0.01, 'lr': learning_rate},
+        {'params': [p for n, p in model.roberta.named_parameters() if not any(nd in n for nd in no_decay) and any(nd in n for nd in group3)],'weight_decay_rate': 0.01, 'lr': learning_rate*2.6},
+        {'params': [p for n, p in model.roberta.named_parameters() if any(nd in n for nd in no_decay) and not any(nd in n for nd in group_all)],'weight_decay_rate': 0.0},
+        {'params': [p for n, p in model.roberta.named_parameters() if any(nd in n for nd in no_decay) and any(nd in n for nd in group1)],'weight_decay_rate': 0.0, 'lr': learning_rate/2.6},
+        {'params': [p for n, p in model.roberta.named_parameters() if any(nd in n for nd in no_decay) and any(nd in n for nd in group2)],'weight_decay_rate': 0.0, 'lr': learning_rate},
+        {'params': [p for n, p in model.roberta.named_parameters() if any(nd in n for nd in no_decay) and any(nd in n for nd in group3)],'weight_decay_rate': 0.0, 'lr': learning_rate*2.6},
+        {'params': [p for n, p in model.named_parameters() if "roberta" not in n], 'lr':2e-5, "momentum" : 0.99},
+    ]
+    return optimizer_parameters
+
+
 def eval_fn(model, data_loader):
     model.eval()
     fin_loss = 0
